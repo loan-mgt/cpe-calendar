f019475f73f0c767d1ccfb81b5b8dc7fc3874dfd
qypol342 <qypol342@gmail.com>
2024-09-03 19:46:00 +0200

    ok

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module cpe/calendar
 
 go 1.23.0
-
-require github.com/arran4/golang-ical v0.3.1
--- request/request.go
+++ request/request.go
@@ -12,7 +12,7 @@ func FetchData() ([]byte, error) {
 	method := "POST"
 
 	// Data to be sent in the POST request
-	data := "javax.faces.partial.ajax=true&javax.faces.source=form%3Aj_idt118&javax.faces.partial.execute=form%3Aj_idt118&javax.faces.partial.render=form%3Aj_idt118&form%3Aj_idt118=form%3Aj_idt118&form%3Aj_idt118_start=1725228000000&form%3Aj_idt118_end=1728684000000&form=form&form%3AlargeurDivCenter=742&form%3AidInit=webscolaapp.Planning_2734064053566000780&form%3Adate_input=02%2F09%2F2024&form%3Aweek=36-2024&form%3Aj_idt118_view=agendaWeek&form%3AoffsetFuseauNavigateur=-7200000&form%3Aonglets_activeIndex=0&form%3Aonglets_scrollState=0&javax.faces.ViewState=6841851136736755168%3A2565943362582303192"
+	data := "javax.faces.partial.ajax=true&javax.faces.source=form%3Aj_idt118&javax.faces.partial.execute=form%3Aj_idt118&javax.faces.partial.render=form%3Aj_idt118&form%3Aj_idt118=form%3Aj_idt118&form%3Aj_idt118_start=1725228000000&form%3Aj_idt118_end=1728684000000&form=form&form%3AlargeurDivCenter=742&form%3AidInit=webscolaapp.Planning_515953417222451125&form%3Adate_input=02%2F09%2F2024&form%3Aweek=36-2024&form%3Aj_idt118_view=agendaWeek&form%3AoffsetFuseauNavigateur=-7200000&form%3Aonglets_activeIndex=0&form%3Aonglets_scrollState=0&javax.faces.ViewState=-2413793382658307369:6268075684027325918"
 
 	req, err := http.NewRequest(method, url, bytes.NewBuffer([]byte(data)))
 	if err != nil {
@@ -30,7 +30,7 @@ func FetchData() ([]byte, error) {
 	req.Header.Add("Origin", "https://mycpe.cpe.fr")
 	req.Header.Add("Connection", "keep-alive")
 	req.Header.Add("Referer", "https://mycpe.cpe.fr/faces/Planning.xhtml")
-	req.Header.Add("Cookie", "JSESSIONID=65175687EBE952D412E1C4245BB92F9C")
+	req.Header.Add("Cookie", "JSESSIONID=58FF76D44D7D2FFC45037DC6DEC307C3")
 	req.Header.Add("Sec-Fetch-Dest", "empty")
 	req.Header.Add("Sec-Fetch-Mode", "cors") // Updated from "no-cors" to "cors" to match the curl
 	req.Header.Add("Sec-Fetch-Site", "same-origin")
