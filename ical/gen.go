1530ff029554985750f1d47fd55f4875f6551c87
qypol342 <qypol342@gmail.com>
2024-09-03 16:17:28 +0200

    good

--- ical/gen.go
+++ ical/gen.go
@@ -8,10 +8,29 @@ import (
 func GenerateICS(events []Event) string {
 	ics := "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Your Organization//Your Product//EN\n"
 
-	for _, event := range events {
-		start, _ := time.Parse(time.RFC3339, event.Start)
-		end, _ := time.Parse(time.RFC3339, event.End)
+	// Define the layout for parsing the datetime with a timezone offset
+	const layout = "2006-01-02T15:04:05-0700"
 
+	for _, event := range events {
+		// Parse the start and end times in the given time zone
+		start, err := time.Parse(layout, event.Start)
+		if err != nil {
+			// Handle parsing error
+			fmt.Println("Error parsing start time:", err)
+			continue
+		}
+		end, err := time.Parse(layout, event.End)
+		if err != nil {
+			// Handle parsing error
+			fmt.Println("Error parsing end time:", err)
+			continue
+		}
+
+		// Convert times to UTC
+		start = start.UTC()
+		end = end.UTC()
+
+		// Format times for ICS
 		ics += "BEGIN:VEVENT\n"
 		ics += fmt.Sprintf("UID:%s\n", event.ID)
 		ics += fmt.Sprintf("DTSTART:%s\n", start.Format("20060102T150405Z"))
