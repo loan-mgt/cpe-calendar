ee6fac6af555cb48f0d4c09a0d97ed89a8191d3a
qypol342 <qypol342@gmail.com>
2024-09-03 16:29:43 +0200

    maybe works

--- ical/gen.go
+++ ical/gen.go
@@ -2,16 +2,41 @@ package ical
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 	"time"
 )
 
+// Regular expression for splitting title into components
+const regexPattern = `(?P<location>.*?)(?P<promo>[1-9][A-Z]{3,})(?P<summary>.*?)(?P<description>(( |n)[A-Z]{3,} .*)|$)`
+
+// GenerateICS generates an ICS string from a list of events
 func GenerateICS(events []Event) string {
 	ics := "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Your Organization//Your Product//EN\n"
 
 	// Define the layout for parsing the datetime with a timezone offset
 	const layout = "2006-01-02T15:04:05-0700"
 
+	// Compile the regular expression
+	re := regexp.MustCompile(regexPattern)
+
 	for _, event := range events {
+		// Remove newline characters from title
+		cleanedTitle := strings.ReplaceAll(event.Title, "\n", " ")
+
+		// Apply regex to split title
+		matches := re.FindStringSubmatch(cleanedTitle)
+		if matches == nil {
+			// Handle case where regex does not match
+			fmt.Println("Error parsing title:", event.Title)
+			continue
+		}
+
+		// Extract components from regex matches
+		location := matches[1]
+		summary := matches[3]
+		description := matches[4]
+
 		// Parse the start and end times in the given time zone
 		start, err := time.Parse(layout, event.Start)
 		if err != nil {
@@ -35,7 +60,9 @@ func GenerateICS(events []Event) string {
 		ics += fmt.Sprintf("UID:%s\n", event.ID)
 		ics += fmt.Sprintf("DTSTART:%s\n", start.Format("20060102T150405Z"))
 		ics += fmt.Sprintf("DTEND:%s\n", end.Format("20060102T150405Z"))
-		ics += fmt.Sprintf("SUMMARY:%s\n", event.Title)
+		ics += fmt.Sprintf("LOCATION:%s\n", location)
+		ics += fmt.Sprintf("SUMMARY:%s\n", summary)
+		ics += fmt.Sprintf("DESCRIPTION:%s\n", description)
 		ics += "END:VEVENT\n"
 	}
 
--- request/request.go
+++ request/request.go
@@ -3,6 +3,7 @@ package request
 import (
 	"bytes"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -11,11 +12,11 @@ func FetchData() ([]byte, error) {
 	method := "POST"
 
 	// Data to be sent in the POST request
-	data := "javax.faces.partial.ajax=true&javax.faces.source=form%3Aj_idt118&javax.faces.partial.execute=form%3Aj_idt118&javax.faces.partial.render=form%3Aj_idt118&form%3Aj_idt118=form%3Aj_idt118&form%3Aj_idt118_start=1725228000000&form%3Aj_idt118_end=1728684000000&form=form&form%3AlargeurDivCenter=687&form%3AidInit=webscolaapp.Planning_375560563831821898&form%3Adate_input=02%2F09%2F2024&form%3Aweek=36-2024&form%3Aj_idt118_view=agendaWeek&form%3AoffsetFuseauNavigateur=-7200000&form%3Aonglets_activeIndex=0&form%3Aonglets_scrollState=0&javax.faces.ViewState=-1435176092611939621%3A358886287956021230"
+	data := "javax.faces.partial.ajax=true&javax.faces.source=form%3Aj_idt118&javax.faces.partial.execute=form%3Aj_idt118&javax.faces.partial.render=form%3Aj_idt118&form%3Aj_idt118=form%3Aj_idt118&form%3Aj_idt118_start=1725228000000&form%3Aj_idt118_end=1728684000000&form=form&form%3AlargeurDivCenter=742&form%3AidInit=webscolaapp.Planning_2734064053566000780&form%3Adate_input=02%2F09%2F2024&form%3Aweek=36-2024&form%3Aj_idt118_view=agendaWeek&form%3AoffsetFuseauNavigateur=-7200000&form%3Aonglets_activeIndex=0&form%3Aonglets_scrollState=0&javax.faces.ViewState=6841851136736755168%3A2565943362582303192"
 
 	req, err := http.NewRequest(method, url, bytes.NewBuffer([]byte(data)))
 	if err != nil {
-		return nil, err
+		log.Fatalf("Failed to create request: %v", err)
 	}
 
 	// Adding headers to the request
@@ -23,18 +24,18 @@ func FetchData() ([]byte, error) {
 	req.Header.Add("Accept", "application/xml, text/xml, */*; q=0.01")
 	req.Header.Add("Accept-Language", "en-US,en;q=0.5")
 	req.Header.Add("Accept-Encoding", "gzip, deflate, br, zstd")
-	req.Header.Add("Referer", "https://mycpe.cpe.fr/faces/Planning.xhtml")
-	req.Header.Add("Origin", "https://mycpe.cpe.fr")
-	req.Header.Add("Connection", "keep-alive")
-	req.Header.Add("Cookie", "JSESSIONID=8AE98E9D4E6E3270432EA926891D4E36")
-	req.Header.Add("Sec-Fetch-Dest", "empty")
-	req.Header.Add("Sec-Fetch-Mode", "no-cors")
-	req.Header.Add("Sec-Fetch-Site", "same-origin")
-	req.Header.Add("TE", "trailers")
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
 	req.Header.Add("Faces-Request", "partial/ajax")
 	req.Header.Add("X-Requested-With", "XMLHttpRequest")
+	req.Header.Add("Origin", "https://mycpe.cpe.fr")
+	req.Header.Add("Connection", "keep-alive")
+	req.Header.Add("Referer", "https://mycpe.cpe.fr/faces/Planning.xhtml")
+	req.Header.Add("Cookie", "JSESSIONID=65175687EBE952D412E1C4245BB92F9C")
+	req.Header.Add("Sec-Fetch-Dest", "empty")
+	req.Header.Add("Sec-Fetch-Mode", "cors") // Updated from "no-cors" to "cors" to match the curl
+	req.Header.Add("Sec-Fetch-Site", "same-origin")
 	req.Header.Add("Priority", "u=0")
+	req.Header.Add("TE", "trailers")
 	req.Header.Add("Pragma", "no-cache")
 	req.Header.Add("Cache-Control", "no-cache")
 
