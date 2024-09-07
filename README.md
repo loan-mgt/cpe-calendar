516feb7aa89634a57b216d853d8855f66500ff4d
qypol342 <qypol342@gmail.com>
2024-09-07 13:56:30 +0200

    add templating

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # cpe-calendar
 Sync your CPE calendar with your personal one
 
+# Setup
 
+1. Frist step is to 
 ```
 openssl genrsa -out secret/private.pem 2048
 openssl rsa -in secret/private.pem -pubout > static/public.pem
--- main.go
+++ main.go
@@ -2,31 +2,59 @@ package main
 
 import (
 	"cpe/calendar/handlers"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
+
+var tpl *template.Template
 
 func init() {
 	// Load environment variables from .env file
 	if err := godotenv.Load(); err != nil {
 		log.Printf("Error loading .env file: %v", err)
 	}
+	// Parse templates
+	tpl = template.Must(template.ParseFiles(filepath.Join("static", "index.html")))
 }
 
 func main() {
 	r := mux.NewRouter()
 
-	// Handle the calendar.ics route with default filename "calendar.ics"
-	r.HandleFunc("/your-cpe-calendar.ics", generate3IRCHandler).Methods("GET")
+	// Serve dynamic index page
+	r.HandleFunc("/", serveIndex).Methods("GET")
 
-	// Serve static files
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("static/")))
+	// Serve static files like JavaScript, CSS, images, etc.
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+
+	// Serve calendar.ics route
+	r.HandleFunc("/your-cpe-calendar.ics", generate3IRCHandler).Methods("GET")
 
 	// Use the router in the http server
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+// serveIndex renders the index.html Go template with environment variables
+func serveIndex(w http.ResponseWriter, r *http.Request) {
+	publicKey, _ := os.ReadFile(filepath.Join("secret", "public.pem"))
+	separator := os.Getenv("SEPARATOR")
+
+	data := struct {
+		PublicKey string
+		Separator string
+	}{
+		PublicKey: string(publicKey),
+		Separator: separator,
+	}
+
+	if err := tpl.Execute(w, data); err != nil {
+		http.Error(w, "Error rendering template", http.StatusInternalServerError)
+	}
 }
 
 // generate3IRCHandler is a wrapper around generateICSHandler that uses a specific filename
